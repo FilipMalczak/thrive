fdae3feb010fa741461ebd5530f4333ea679baa6
Filip <filip.malczak@gmail.com>
2019-06-15 16:52:43 +0200

    Fixed mismatched step images in CircleCI

--- .circleci/docker_build.sh
+++ .circleci/docker_build.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 # THIS SCRIPT ASSUMES THAT JAR FILES WERE ALREADY GENERATED, PROBABLY BY ./gradlew clean build
+# AS WELL AS THE ACCOMPANYING TASKS (listDockerizedProjectsDirectories, generateDockerfile,
+# writeVersion) WERE ALREADY EXECUTED
 
 HERE="$(realpath $(dirname "$0"))"
 ROOT_DIR=$HERE/..
 
-$ROOT_DIR/gradlew listDockerizedProjectsDirectories
-
 for NAME in $(cat $ROOT_DIR/build/dockerizedProjects.txt)
 do
-    $ROOT_DIR/gradlew :$NAME:generateDockerfile :$NAME:writeVersion
-    VERSION=$(cat $ROOT_DIR/$NAME/build/generated/meta/version.txt)
     docker build $ROOT_DIR/$NAME -t filipmalczak/$NAME:$VERSION
 done
---
+++ .circleci/prepare_docker_build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+HERE="$(realpath $(dirname "$0"))"
+ROOT_DIR=$HERE/..
+
+$ROOT_DIR/gradlew listDockerizedProjectsDirectories
+
+for NAME in $(cat $ROOT_DIR/build/dockerizedProjects.txt)
+do
+    $ROOT_DIR/gradlew :$NAME:generateDockerfile :$NAME:writeVersion
+    VERSION=$(cat $ROOT_DIR/$NAME/build/generated/meta/version.txt)
+done
