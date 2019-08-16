409cc72c145ae1403ecd0d9f785b4663b1543d28
Filip <filip.malczak@gmail.com>
2019-08-16 20:23:54 +0200

    Fix Docker image pushing

--- .circleci/push_docker_images.sh
+++ .circleci/push_docker_images.sh
@@ -8,9 +8,9 @@ echo "Logging in to DockerHub with username $DOCKER_USERNAME"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 set -x
 
-ADMIN_VERSION=$(cat $ROOT_DIR/thrive-admin/build/generated/meta/version.txt)
-DOCS_VERSION=$(cat $ROOT_DIR/thrive-docs/build/generated/meta/version.txt)
-GATEWAY_VERSION=$(cat $ROOT_DIR/thrive-gateway/build/generated/meta/version.txt)
+ADMIN_VERSION=$(cat $ROOT_DIR/thrive-admin/build/thrive/metadata/version.txt)
+DOCS_VERSION=$(cat $ROOT_DIR/thrive-docs/build/thrive/metadata/version.txt)
+GATEWAY_VERSION=$(cat $ROOT_DIR/thrive-gateway/build/thrive/metadata/version.txt)
 
 docker push thriveframework/thrive-admin:$ADMIN_VERSION
 docker push thriveframework/thrive-docs:$DOCS_VERSION
